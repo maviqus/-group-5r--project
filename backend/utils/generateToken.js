dd171b25fc54c70386694e396af0fbaf2e3add27
Nguyen Xuan Bach <user-xuanbach@example.com>
2025-10-24 22:36:18 +0700

    Update auth files on main

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET || 'secret', { expiresIn: '1h' });
+    return jwt.sign({ id }, process.env.JWT_SECRET || 'secret', { expiresIn: '1h' });
 };
 
 module.exports = generateToken;
