b55d9522d04069ff843b9853ecc7652375d47a29
Nguyen Xuan Bach <user-xuanbach@example.com>
2025-10-26 02:20:03 +0700

    Fix: Change route from / to /users for admin API

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -5,7 +5,7 @@ const { protect, admin } = require('../middlewares/auth.middleware');
 
 // Routes
 router.route('/profile').get(protect, getProfile).put(protect, updateProfile);
-router.route('/').get(protect, admin, getAllUsers);
-router.route('/:id').delete(protect, admin, deleteUser);
+router.route('/users').get(protect, admin, getAllUsers);
+router.route('/users/:id').delete(protect, admin, deleteUser);
 
 module.exports = router;
